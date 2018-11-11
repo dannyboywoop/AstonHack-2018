43d28dd5c991f14c6f295f0cf4cb8152ff2dc1da
Daniel Holmes <DanielJholmes@hotmail.co.uk>
2018-11-11 10:40:09 +0000

    Moving Enemies added

---
+++ AstonHack-2018/Assets/Scripts/enemySpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawner : MonoBehaviour {
+
+    public GameObject enemyPreFab;
+    public GameObject goal;
+
+    void Start()
+    {
+        this.tag = "spawn";  
+    }
+
+    // Use this for initialization
+    public void spawnEnemy() {
+        GameObject agent = Instantiate(enemyPreFab, this.transform.position, Quaternion.identity);
+        agent.GetComponent<EnemyAI>().goal = goal.transform;
+	}
+	
+}
