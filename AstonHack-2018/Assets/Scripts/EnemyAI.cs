618abade35384fdfbd18b0dacd96242a72fdf378
Matthew Dewhurst <mat.dewy@gmail.com>
2018-11-11 12:07:16 +0000

    Final changes

--- AstonHack-2018/Assets/Scripts/EnemyAI.cs
+++ AstonHack-2018/Assets/Scripts/EnemyAI.cs
@@ -6,11 +6,19 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour {
 
     public Transform goal;
-    
+    public GameObject GameOverText;
+
 	// Use this for initialization
 	void Start () {
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
         agent.SetDestination(goal.position);
+    }
+
+    private void Update() {
+        if((transform.position - goal.position).magnitude < 2.0f) {
+            GameOverText.SetActive(true);
+            Destroy(transform.gameObject);
+        }
     }
 
 }
