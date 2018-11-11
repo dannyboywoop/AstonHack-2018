a52e60567c980a4300db7774f13829468e6f7c9b
Edward Evans <tardisman5197@gmail.com>
2018-11-11 10:57:42 +0000

    Added assets

---
+++ AstonHack-2018/Assets/Scripts/Health.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	public int maxHealth = 100;
+
+	public int currentHealth;
+
+	// Use this for initialization
+	void Start () {
+		currentHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentHealth <= 0) {
+			// Play death animation
+			// ToDo
+
+			// Destroy Game object
+			Destroy(transform.parent.gameObject);
+		}
+	}
+
+	public void damage(int damageValue) {
+		currentHealth -= damageValue;
+	}
+}
