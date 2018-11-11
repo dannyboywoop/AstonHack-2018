5eb34957fa7de5cfd5332434fafd4fa16d79311d
Matthew Dewhurst <mat.dewy@gmail.com>
2018-11-11 09:37:44 +0000

    Script and button

---
+++ AstonHack-2018/Assets/Scripts/Catapult.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catapult : MonoBehaviour {
+
+    private Vector3 CatapultStartPosition;
+    private Vector3 CatapultReleasePosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
